fa7f22a15d138a65b1a7dbfeeb670a4e4449bbc3
Alejandro Tesán <tesan.deale22@zaragoza.salesianos.edu>
2024-03-11 12:56:12 +0100

    subiendo cosas de testeo

---
+++ TESTs/demo_springboot/src/main/java/com/example/demo/controller/java/Message.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller.java;
+
+public class Message {
+
+	// los atributos propios de la clase
+	private Integer idMessage;
+
+	// Metodos constructores (completo y vacio
+	public Message(Integer idMessage) {
+		this.idMessage = idMessage;
+	}
+
+	public Message() {
+	}
+
+	// getter y setter
+	public Integer getIdMessage() {
+		return idMessage;
+	}
+
+	public void setIdMessage(Integer idMessage) {
+		this.idMessage = idMessage;
+	}
+
+}
